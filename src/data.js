79b44724a7bbc8f596f83bcac7bd8f787e369645
sankar mukesh <ysankarmukesh@gmail.com>
2022-10-25 10:15:24 +0530

    added

--- src/data.js
+++ src/data.js
@@ -1,32 +1,32 @@
 export const products = [
   {
     id: 1,
-    img: "https://res.cloudinary.com/dqajigt6s/main_logo_vheqme_j3hi9m.png",
+    img: "https://res.cloudinary.com/dqajigt6s/image/upload/v1666672996/HTML_os3hcp.png",
     
   },
   {
     id: 2,
-    img: "https://res.cloudinary.com/dqajigt6s/main_logo_vheqme_j3hi9m.png",
+    img: "https://res.cloudinary.com/dqajigt6s/image/upload/v1666672997/CSS_lzqmr2.png",
     
   },
   {
     id: 3,
-    img: "https://res.cloudinary.com/dqajigt6s/main_logo_vheqme_j3hi9m.png",
+    img: "https://res.cloudinary.com/dqajigt6s/image/upload/v1666672996/JS_pls6od.png",
     
   },
   {
     id: 4,
-    img: "https://res.cloudinary.com/dqajigt6s/main_logo_vheqme_j3hi9m.png",
+    img: "https://res.cloudinary.com/dqajigt6s/image/upload/v1666672996/GIT_agadg1.png",
     
   },
   {
     id: 5,
-    img: "https://res.cloudinary.com/dqajigt6s/main_logo_vheqme_j3hi9m.png",
+    img: "https://res.cloudinary.com/dqajigt6s/image/upload/v1666672997/REACT_ahkba9.png",
     
   },
   {
     id: 6,
-    img: "https://res.cloudinary.com/dqajigt6s/main_logo_vheqme_j3hi9m.png",
+    img: "https://res.cloudinary.com/dqajigt6s/image/upload/v1666672996/Django_ctspea.png",
     
   },
 ];
